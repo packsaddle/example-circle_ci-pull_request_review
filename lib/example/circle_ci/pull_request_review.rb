94e5288f2ba07cbd4925b0fb9f0a11abf479a179
sanemat <o.gata.ken@gmail.com>
2015-02-26 01:40:43 +0900

    chore(invalid): add too long line

--- lib/example/circle_ci/pull_request_review.rb
+++ lib/example/circle_ci/pull_request_review.rb
@@ -3,6 +3,9 @@ require 'example/circle_ci/pull_request_review/version'
 module Example
   module CircleCi
     module PullRequestReview
+      def too_long
+        'tttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt'
+      end
       # Your code goes here...
     end
   end
