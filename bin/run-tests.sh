4d0dfd035e19e5799bf958659f4e3537361878ab
sanemat <o.gata.ken@gmail.com>
2015-02-26 01:39:26 +0900

    chore(circleci): add script

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -v
-if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-  # Travis-CI
+if [ -n "${CI_PULL_REQUEST}" ]; then
+  # Circle-CI
   #
-  # git clone --depth=50 \
-  # git://github.com/packsaddle/example-ruby-travis-ci.git \
-  # packsaddle/example-ruby-travis-ci
-  # cd packsaddle/example-ruby-travis-ci
-  # git fetch origin +refs/pull/1/merge:
-  # git checkout -qf FETCH_HEAD
+
+  git branch
+  git remote show
+  git remote show origin
 
   gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
               checkstyle_filter-git saddler saddler-reporter-github
@@ -41,4 +39,4 @@ if [ -n "${TRAVIS_PULL_REQUEST}" ] && [ "${TRAVIS_PULL_REQUEST}" != "false" ]; t
       --require saddler/reporter/github \
       --reporter Saddler::Reporter::Github::PullRequestReviewComment
 fi
-bundle exec rake
+exit 0
